2bae47cf26dd22871399748fed074f7611665165
fatehahmod <ahmod.fateh@gmail.com>
2021-09-09 00:29:45 +0600

    add new

--- index.js
+++ index.js
@@ -32,4 +32,76 @@ const ask=()=>{
         console.log('hi')
     }
 
+};
+// alert 
+function func(){
+    swal("Hello world!");
+    
 }
+function func2(){
+    swal({
+        title: "Good job!",
+        text: "You clicked the button!",
+        icon: "success",
+        button: "Submit",
+      });
+}
+function func3(){
+    
+    swal("Good job!", "You clicked the button!", "success");
+}
+function func4(){
+    
+    swal("Good job!", "You clicked the button!", "error");
+}
+function func5(){
+    
+    swal("Click on either the button or outside the modal.")
+    .then((value) => {
+      swal(`The returned value is: ${value}`);
+    });
+}
+function func6(){
+    
+    swal({
+        title: "Are you sure?",
+        text: "Once deleted, you will not be able to recover this imaginary file!",
+        icon: "warning",
+        buttons: true,
+        dangerMode: true,
+      })
+      .then((willDelete) => {
+        if (willDelete) {
+          swal("Poof! Your imaginary file has been deleted!", {
+            icon: "success",
+          });
+        } else {
+          swal("Your imaginary file is safe!");
+        }
+      });
+}
+function func7(){
+    
+    swal({
+        title: "Are you sure?",
+        text: "Once deleted, you will not be able to recover this imaginary file!",
+        icon: "warning",
+        buttons: true,
+        dangerMode: true,
+      })
+      .then((willDelete) => {
+        if (willDelete) {
+          swal("Poof! Your imaginary file has been deleted!", {
+            icon: "success",
+          });
+        } else {
+          swal("Your imaginary file is safe!");
+        }
+      });
+}
+
+
+
+const colors = {mango: 'green', grapes: 'black', organe: 'yellow'};
+
+console.log(colors[grapes])
